d80ed944caf0aeb7ea333f7442e9adfeb9b070a8
IvMollov <mollov.i@abv.bg>
2016-12-08 01:05:08 +0200

    Nodes Rs.

--- Problem No.10 Notes of Rs/Program.cs
+++ Problem No.10 Notes of Rs/Program.cs
@@ -12,35 +12,67 @@ namespace Problem_No._10_Notes_of_Rs
         {
             Console.Write("Input amount: ");
             int amount = int.Parse(Console.ReadLine());
-            int fiveHundred = 500;
-            int oneHundred = 100;
-            int fifty = 50;
-            int twenty = 20;
-            int ten = 10;
-            int five = 5;
-            int two = 2;
-            int one = 1;
-            int nu = ((int)amount / fiveHundred) / oneHundred;
-            Console.WriteLine(nu);
-            if ((int)(amount / fiveHundred) >= 1)
-            {
-                Console.WriteLine("500:{0}", (int)amount / fiveHundred);
-                if ((((int)amount / fiveHundred) / oneHundred) > -1)
-                {
-                    Console.WriteLine("100:{0}", ((int)amount / fiveHundred) / oneHundred);
-                }
-                if (((int)amount / fiveHundred) > 0)
-                {
-                    Console.WriteLine("50:{0}", ((int)amount / fiveHundred));
-                }
-           
-            }
-            
-            else if (((int)amount / oneHundred) > -1)
-            {
-                Console.WriteLine("100:{0}", (int)amount / oneHundred);
-            }
+            int fiveHundred;
+            int oneHundred;
+            int fifty;
+            int twenty;
+            int ten;
+            int five;
+            int two;
+            int one;
 
+            if (amount >= 500)
+            {
+                fiveHundred = amount / 500;
+                amount -= (fiveHundred * 500);
+                oneHundred = amount / 100;
+                amount -= (oneHundred * 100);
+                fifty = amount / 50;
+                amount -= (fifty * 50);
+                twenty = amount / 20;
+                amount -= (twenty * 20);
+                ten = amount / 10;
+                amount -= (ten * 10);
+                five = amount / 5;
+                amount -= (five * 5);
+                two = amount / 2;
+                amount -= (two * 2);
+                one = amount / 1;
+                    
+                Console.WriteLine("500:{0}", fiveHundred);
+                Console.WriteLine("100:{0}", oneHundred);
+                Console.WriteLine("50:{0}", fifty);
+                Console.WriteLine("20:{0}", twenty);
+                Console.WriteLine("10:{0}", ten);
+                Console.WriteLine("5:{0}", five);
+                Console.WriteLine("2:{0}", two);
+                Console.WriteLine("1:{0}", one);
+            }
+            else if (amount < 500)
+            {
+                oneHundred = amount / 100;
+                amount -= (oneHundred * 100);
+                fifty = amount / 50;
+                amount -= (fifty * 50);
+                twenty = amount / 20;
+                amount -= (twenty * 20);
+                ten = amount / 10;
+                amount -= (ten * 10);
+                five = amount / 5;
+                amount -= (five * 5);
+                two = amount / 2;
+                amount -= (two * 2);
+                one = amount / 1;
+
+                Console.WriteLine("100:{0}", oneHundred);
+                Console.WriteLine("50:{0}", fifty);
+                Console.WriteLine("20:{0}", twenty);
+                Console.WriteLine("10:{0}", ten);
+                Console.WriteLine("5:{0}", five);
+                Console.WriteLine("2:{0}", two);
+                Console.WriteLine("1:{0}", one);
+            }
+          
         }
     }
 }
