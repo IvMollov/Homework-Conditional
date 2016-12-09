c9214d57278c4d058a2c1eb01b929cf0e8c0907e
IvMollov <mollov.i@abv.bg>
2016-12-09 23:04:09 +0200

    Conditional

---
+++ Problem 12/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_12
+{
+    class Program
+    {
+        const string HUNDREDS = "Hundred";
+        static void Main(string[] args)
+        {
+            string value = Console.ReadLine();
+            int number = 0;
+            if(!int.TryParse(value, out number))
+            {
+                Console.WriteLine("Not a Number!");
+
+            }
+            else
+            {
+                string[] digits = new string[] { "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine" };
+                string[] tens = new string[] { "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety" };
+                string[] exceptionNumbers = new string[] { "Ten", "Eleven", "Twelve", "Thirteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen" };
+                if (number == 0)
+                {
+                    Console.WriteLine("Zero");
+                }
+
+                List<int> numberDigits = new List<int>();
+
+                for (; number != 0; number /= 10)
+                {
+                    int digit = number % 10;
+                    numberDigits.Add(digit);
+                }
+                numberDigits.Reverse();
+
+                int numberLength = numberDigits.Count;
+                switch (numberLength)
+                {
+                    case 1:
+                        Console.WriteLine(digits[numberDigits[0] + 1]);
+                        break;
+                    case 2:
+                        if(numberDigits[1] == 1)
+                        {
+                            Console.WriteLine(exceptionNumbers[numberDigits[1]]);
+                        }
+                        else
+                        {
+                            Console.WriteLine(tens[numberDigits[0]] + " " + digits[numberDigits[1]]);
+                        }
+                        break;
+                    case 3:
+
+                }
+
+            }
+        }
+    }
+}
--- Problem No.5 Zero subset/Program.cs
+++ Problem No.5 Zero subset/Program.cs
@@ -10,110 +10,193 @@ namespace Problem_No._4_Zero_subset
     {
         static void Main(string[] args)
         {
-            Console.Write("Enter number 1: ");
-            int numberOne = int.Parse(Console.ReadLine());
-            Console.Write("Enter number 2: ");
-            int numberTwo = int.Parse(Console.ReadLine());
-            Console.Write("Enter number 3: ");
-            int numberThree = int.Parse(Console.ReadLine());
-            Console.Write("Enter number 4: ");
-            int numberFour = int.Parse(Console.ReadLine());
-            Console.Write("Enter number 5: ");
-            int numberFive = int.Parse(Console.ReadLine());
-            if ((numberOne + numberTwo) == 0)
+            //Console.Write("Enter number 1: ");
+            //int numberOne = int.Parse(Console.ReadLine());
+            //Console.Write("Enter number 2: ");
+            //int numberTwo = int.Parse(Console.ReadLine());
+            //Console.Write("Enter number 3: ");
+            //int numberThree = int.Parse(Console.ReadLine());
+            //Console.Write("Enter number 4: ");
+            //int numberFour = int.Parse(Console.ReadLine());
+            //Console.Write("Enter number 5: ");
+            //int numberFive = int.Parse(Console.ReadLine());
+            //if ((numberOne + numberTwo) == 0)
+            //{
+            //    Console.WriteLine("{0} + {1} = 0", numberOne, numberTwo);
+            //}
+            //else if ((numberOne + numberThree) == 0)
+            //{
+            //    Console.WriteLine("{0} + {1} = 0", numberOne, numberThree);
+            //}
+            //else if ((numberOne + numberFour) == 0)
+            //{
+            //    Console.WriteLine("{0} + {1} = 0", numberOne, numberFour);
+            //}
+            //else if ((numberOne + numberFive) == 0)
+            //{
+            //    Console.WriteLine("{0} + {1} = 0", numberOne, numberFive);
+            //}
+            //else if ((numberTwo + numberThree) == 0)
+            //{
+            //    Console.WriteLine("{0} + {1} = 0", numberTwo, numberThree);
+            //}
+            //else if ((numberTwo + numberFour) == 0)
+            //{
+            //    Console.WriteLine("{0} + {1} = 0", numberTwo, numberFour);
+            //}
+            //else if ((numberTwo + numberFive) == 0)
+            //{
+            //    Console.WriteLine("{0} + {1} = 0", numberTwo, numberFive);
+            //}
+            //else if ((numberThree + numberFour) == 0)
+            //{
+            //    Console.WriteLine("{0} + {1} = 0", numberThree, numberFour);
+            //}
+            //else if ((numberThree + numberFive) == 0)
+            //{
+            //    Console.WriteLine("{0} + {1} = 0", numberThree, numberFive);
+            //}
+            //else if ((numberFour + numberFive) == 0)
+            //{
+            //    Console.WriteLine("{0} + {1} = 0", numberFour, numberFive);
+            //}
+            //else if ((numberOne + numberTwo + numberThree) == 0)
+            //{
+            //    Console.WriteLine("{0} + {1} + {2} = 0", numberOne, numberTwo, numberThree);
+            //}
+            //else if ((numberOne + numberTwo + numberFour) == 0)
+            //{
+            //    Console.WriteLine("{0} + {1} + {2} = 0", numberOne, numberTwo, numberFour);
+            //}
+            //else if ((numberOne + numberTwo + numberFive) == 0)
+            //{
+            //    Console.WriteLine("{0} + {1} + {2} = 0", numberOne, numberTwo, numberFive);
+            //}
+            //else if ((numberOne + numberThree + numberFour) == 0)
+            //{
+            //    Console.WriteLine("{0} + {1} + {2} = 0", numberOne, numberThree, numberFour);
+            //}
+            //else if ((numberOne + numberThree + numberFive) == 0)
+            //{
+            //    Console.WriteLine("{0} + {1} + {2} = 0", numberOne, numberThree, numberFive);
+            //}
+            //else if ((numberOne + numberFour + numberFive) == 0)
+            //{
+            //    Console.WriteLine("{0} + {1} + {2} = 0", numberOne, numberFour, numberFive);
+            //}
+            //else if ((numberTwo + numberThree + numberFour) == 0)
+            //{
+            //    Console.WriteLine("{0} + {1} + {2} = 0", numberTwo, numberThree, numberFour);
+            //}
+            //else if ((numberTwo + numberFour + numberFive) == 0)
+            //{
+            //    Console.WriteLine("{0} + {1} + {2} = 0", numberTwo, numberFour, numberFive);
+            //}
+            //else if ((numberThree + numberFour + numberFive) == 0)
+            //{
+            //    Console.WriteLine("{0} + {1} + {2} = 0", numberThree, numberFour, numberFive);
+            //}
+            //else if ((numberOne + numberTwo + numberThree + numberFour) == 0)
+            //{
+            //    Console.WriteLine("{0} + {1} + {2} + {3}= 0", numberOne, numberTwo, numberThree, numberFour);
+            //}
+            //else if ((numberOne + numberTwo + numberThree + numberFive) == 0)
+            //{
+            //    Console.WriteLine("{0} + {1} + {2} + {3}= 0", numberOne, numberTwo, numberThree, numberFive);
+            //}
+            //else if ((numberTwo + numberThree + numberFour + numberFive) == 0)
+            //{
+            //    Console.WriteLine("{0} + {1} + {2} + {3}= 0", numberTwo, numberThree, numberFour, numberFive);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("There is no subset with 0 sum.");
+            //}
+
+            //Console.ReadLine();
+
+            bool isFound = false;
+            int sum = 0;
+            int count = 0;
+            int[] arr = new int[5];
+            int[] newArr = new int[5];
+            for (int i = 0; i < arr.Length; i++)
             {
-                Console.WriteLine("{0} + {1} = 0", numberOne, numberTwo);
+                Console.Write("Enter number {0}: ", i + 1);
+                arr[i] = int.Parse(Console.ReadLine());
             }
-            else if ((numberOne + numberThree) == 0)
+
+            for (int i = 0; i < arr.Length; i++)
             {
-                Console.WriteLine("{0} + {1} = 0", numberOne, numberThree);
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (!isFound)
+                    {
+                        if ((arr[j] + arr[i]) == sum)
+                        {
+                            newArr[i] = arr[i];
+                            newArr[j] = arr[j];
+                            count = 2;
+                            isFound = true;
+                            break;
+                        }
+                    }
+                    for (int k = j + 1; k < arr.Length; k++)
+                    {
+                        if (!isFound)
+                        {
+                            if ((arr[j] + arr[i] + arr[k]) == sum)
+                            {
+                                newArr[i] = arr[i];
+                                newArr[j] = arr[j];
+                                newArr[k] = arr[k];
+                                count = 3;
+                                isFound = true;
+                                break;
+                            }
+                        }
+                        for (int n = k + 1; n < arr.Length; n++)
+                        {
+                            if (!isFound)
+                            {
+                                if ((arr[j] + arr[i] + arr[k] + arr[n]) == sum)
+                                {
+                                    newArr[i] = arr[i];
+                                    newArr[j] = arr[j];
+                                    newArr[k] = arr[k];
+                                    newArr[n] = arr[n];
+                                    count = 4;
+                                    isFound = true;
+                                    break;
+                                }
+                            }
+
+                        }
+                    }
+                }
             }
-            else if ((numberOne + numberFour) == 0)
+            if (isFound)
             {
-                Console.WriteLine("{0} + {1} = 0", numberOne, numberFour);
-            }
-            else if ((numberOne + numberFive) == 0)
-            {
-                Console.WriteLine("{0} + {1} = 0", numberOne, numberFive);
-            }
-            else if ((numberTwo + numberThree) == 0)
-            {
-                Console.WriteLine("{0} + {1} = 0", numberTwo, numberThree);
-            }
-            else if ((numberTwo + numberFour) == 0)
-            {
-                Console.WriteLine("{0} + {1} = 0", numberTwo, numberFour);
-            }
-            else if ((numberTwo + numberFive) == 0)
-            {
-                Console.WriteLine("{0} + {1} = 0", numberTwo, numberFive);
-            }
-            else if ((numberThree + numberFour) == 0)
-            {
-                Console.WriteLine("{0} + {1} = 0", numberThree, numberFour);
-            }
-            else if ((numberThree + numberFive) == 0)
-            {
-                Console.WriteLine("{0} + {1} = 0", numberThree, numberFive);
-            }
-            else if ((numberFour + numberFive) == 0)
-            {
-                Console.WriteLine("{0} + {1} = 0", numberFour, numberFive);
-            }
-            else if ((numberOne + numberTwo + numberThree) == 0)
-            {
-                Console.WriteLine("{0} + {1} + {2} = 0", numberOne, numberTwo, numberThree);
-            }
-            else if ((numberOne + numberTwo + numberFour) == 0)
-            {
-                Console.WriteLine("{0} + {1} + {2} = 0", numberOne, numberTwo, numberFour);
-            }
-            else if ((numberOne + numberTwo + numberFive) == 0)
-            {
-                Console.WriteLine("{0} + {1} + {2} = 0", numberOne, numberTwo, numberFive);
-            }
-            else if ((numberOne + numberThree + numberFour) == 0)
-            {
-                Console.WriteLine("{0} + {1} + {2} = 0", numberOne, numberThree, numberFour);
-            }
-            else if ((numberOne + numberThree + numberFive) == 0)
-            {
-                Console.WriteLine("{0} + {1} + {2} = 0", numberOne, numberThree, numberFive);
-            }
-            else if ((numberOne + numberFour + numberFive) == 0)
-            {
-                Console.WriteLine("{0} + {1} + {2} = 0", numberOne, numberFour, numberFive);
-            }
-            else if ((numberTwo + numberThree + numberFour) == 0)
-            {
-                Console.WriteLine("{0} + {1} + {2} = 0", numberTwo, numberThree, numberFour);
-            }
-            else if ((numberTwo + numberFour + numberFive) == 0)
-            {
-                Console.WriteLine("{0} + {1} + {2} = 0", numberTwo, numberFour, numberFive);
-            }
-            else if ((numberThree + numberFour + numberFive) == 0)
-            {
-                Console.WriteLine("{0} + {1} + {2} = 0", numberThree, numberFour, numberFive);
-            }
-            else if ((numberOne + numberTwo + numberThree + numberFour) == 0)
-            {
-                Console.WriteLine("{0} + {1} + {2} + {3}= 0", numberOne, numberTwo, numberThree, numberFour);
-            }
-            else if ((numberOne + numberTwo + numberThree + numberFive) == 0)
-            {
-                Console.WriteLine("{0} + {1} + {2} + {3}= 0", numberOne, numberTwo, numberThree, numberFive);
-            }
-            else if ((numberTwo + numberThree + numberFour + numberFive) == 0)
-            {
-                Console.WriteLine("{0} + {1} + {2} + {3}= 0", numberTwo, numberThree, numberFour, numberFive);
+                for (int i = 0; i < newArr.Length; i++)
+                {
+                    if(newArr[i] == 0)
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        Console.Write("{0} + ", newArr[i]);
+                    }
+
+                }
+                Console.Write("= 0. Zero subset was found!");
             }
             else
             {
-                Console.WriteLine("There is no subset with 0 sum.");
+                Console.Write("No zero subset was found.");
             }
-
             Console.ReadLine();
         }
+
     }
 }
